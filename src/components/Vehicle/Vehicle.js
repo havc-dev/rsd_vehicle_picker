ee50377251de1da75fb3b32f7c67578b11615715
havc-dev <havc.developer@gmail.com>
2021-08-28 15:37:49 -0500

    google admob

--- src/components/Vehicle/Vehicle.js
+++ src/components/Vehicle/Vehicle.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import VehicleDetails from "./VehicleDetails";
 import ToggleButton from '../Buttons/ToggleButton'
 import VehicleBalance from "./VehicleBalance";
+import Delete from "../Icons/Delete";
 
 const Vehicle = ({ vehicle, drivingInfo }) => {
   const [showVehicleDetails, setShowVehicleDetails] = useState(false);
@@ -14,10 +15,16 @@ const Vehicle = ({ vehicle, drivingInfo }) => {
     setShowVehicleBalance(!showVehicleBalance);
     setShowVehicleDetails(false)
   };
+  const deleteVehicleHandler = () => {
+    console.log('borrar')
+  }
   return (
     <li className="w-full">
       <div className="flex flex-col">
-        <h3>{vehicleBasics}</h3>
+        <div className="flex align-middle mx-auto">
+          <h3>{vehicleBasics}</h3>
+          <Delete color="red" width="20px" onClick={deleteVehicleHandler} />
+        </div>
         <ToggleButton text="Vehicle details" onClick={cardToggleHandler}/>
         <ToggleButton text="Vehicle balance" onClick={balanceToggleHandler}/>
       </div>
